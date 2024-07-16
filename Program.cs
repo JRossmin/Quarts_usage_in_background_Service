b74fcb087d500d234adabf7eaf5dd9338c9fe43d
JRossmin <jrosalesg98@gmail.com>
2024-07-16 00:01:37 -0600

    First usage with quartz in background service
    A background service for Windows that uses Quartz in order to use schedulers to execute them within different projects of the entire solution.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Microsoft.Extensions.DependencyInjection;
+namespace WServiceTest_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var services = ConfigureServices();
+
+            var rc = HostFactory.Run(x =>                                   //1
+            {
+                x.Service<ServiceTest>(s =>                                   //2
+                {
+                    s.ConstructUsing(name => services.GetService<ServiceTest>());               //3
+                    s.WhenStarted(tc => tc.Start());                         //4
+                    s.WhenStopped(tc => tc.Stop());                          //5
+                });
+                x.RunAsLocalSystem();                                       //6
+
+                x.SetDescription("MyService");                   //7
+                x.SetDisplayName("My Service");                                  //8
+                x.SetServiceName("This is a sample Topshelf service.");                                  //9
+            });                                                             //10
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  //11
+            Environment.ExitCode = exitCode;
+
+
+
+        }
+        private static ServiceProvider ConfigureServices()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            // Register services and dependencies here
+            //serviceCollection.AddSingleton<ICOM_LogFile, COM_LogFileHandler>();
+            serviceCollection.AddSingleton<ServiceTest>();
+            //serviceCollection.AddSingleton<ITimerService, TimerService>();
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
---
+++ ServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using QuartzLibrary;
+using System;
+using Countercsharp;
+using System.Threading.Tasks;
+
+namespace WServiceTest_2
+{
+    public class ServiceTest
+    {
+        
+        public ServiceTest() { }
+        public void Start()
+        {
+
+            Console.WriteLine("HOLA");
+             JobSchedulerManager.Instance.InitializeAsync().GetAwaiter().GetResult();
+            var count = new Countercsharp.CounterTest();
+            var countvb = new VBLibrary.Counter();
+            count.Counter();
+            countvb.Counter();
+
+        }
+        public  void Stop()
+        {
+             JobSchedulerManager.Instance.ShutdownAsync().GetAwaiter().GetResult();
+        }
+    }
+}
